45197609b389e3d845a3bacdc483d96341173ca2
ahmadjamalalkhatib <78502707+ahmadjamalalkhatib@users.noreply.github.com>
2022-11-15 01:30:08 -0500

    Done!
    should be done try 2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,69 @@
-import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
+import React, { Component }  from 'react';
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
-  item.image = process.env.PUBLIC_URL + "/" + item.image;
+    item.image = process.env.PUBLIC_URL + "/" + item.image;
 });
 /* ############################################################## */
 
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
+    // TODO: use useState to create a state variable to hold the state of the cart
+    /* add your cart state code here */
 
-  return (
-    <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+    const[cart, updateStateCart] = useState([])
+    const addToCart = (i) => {
+        const currCart = Object.assign({}, cart)
+        if (currCart[i]) {
+            currCart[i] = currCart[i] + 1
+        } else {
+            currCart[i] = 1
+        }
+        console.log(cart[i])
+        updateStateCart(currCart)
+    }
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+    const totalPrice = () => {
+        let tot = 0
+        for (var key in cart) {
+            tot = cart[key] * bakeryData[key].price
+        }
+        return tot
+    }
 
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Welcome to My Bakery </h1> {/* TODO: personalize your bakery (if you want) */}
+            <div style={{display: "flex", flexDirection: "row"}}>
+                <div style={{width: "70%"}}>
+                    <Row xs={1} md={2} className="g-4">
+                        {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+                            <Col>
+                                <BakeryItem name = {item.name} desc={item.desc} price={item.price} image={item.image} index = {index} item = {item} addToCart={addToCart}/>
+                            </Col>
+                        ))}
+                    </Row>
+                </div>
+
+                <div style={{width: "30%"}}>
+                    <h2>Cart</h2>
+                    {
+                        Object.entries(cart)
+                            .map(([key, value]) =>
+                                <div> {value}x {bakeryData[key].name} </div>)
+                    }
+                    <div> Total: {totalPrice()} </div>
+
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
